f0cfc7e430286e9a5cdd9370f6a58f68d01ffa68
e-coders <aasheeshsaumya@gmail.com>
2021-10-21 23:23:28 +0530

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -1,12 +1,3 @@
-var allowSubmit = false;
-function capcha_filled(){
-    allowSubmit = true;
-}
-function capcha_expired(){
-    allowSubmit = false;
-}
-function check_if_capcha_is_filled(e){
-    if(allowSubmit) return true;
-    e.preventDefault();
-    alert('Fight us and defeat viruses by saying "I am not a robot!!"!!');
+window.removeFakeCaptcha = function() {
+   document.querySelector('.captcha-fake-field').remove();
 }
