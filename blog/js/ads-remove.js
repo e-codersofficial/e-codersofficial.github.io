a1bc17e5b4c28336e53e25c059d688a0d6e5bcbe
E - Coders (Aasheesh Agarwal) <aasheeshsaumya@gmail.com>
2022-05-01 19:08:12 +0530

    Delete blog directory

--- blog/js/ads-remove.js
+++
@@ -1,5 +0,0 @@
-f = document.querySelector("#ads");
-c = f.innerHTML;
-if(c.includes("<!--NO-->")){
-	f.innerHTML = "<span class='red'>There are no posts, posted by the admin.";
-}
--- blog/js/fixBlank.js
+++
@@ -1,2 +0,0 @@
-c = document.querySelector("#index");
-c.innerHTML = '<span class="red">There are no posts posted by the admin.</span>';
